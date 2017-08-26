fffffb79c7bf9feb1fdb2573ebe769868f93cc6a
Zekx <Vanarid@gmail.com>
2017-08-26 11:18:14 -0700

    Added additional admin functions.

---
+++ bungWeb/Controllers/PostEdit/EditController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace bungWeb.Controllers.PostEdit
+{
+    public class EditController : Controller
+    {
+        [HttpGet]
+        public ActionResult EditPost()
+        {
+            ViewData["logged"] = false;
+
+            if (Session["user"] != null)
+            {
+                ViewData["logged"] = true;
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RemovePost() {
+            ViewData["logged"] = false;
+
+            if (Session["user"] != null)
+            {
+                ViewData["logged"] = true;
+            }
+
+            return View();
+        }
+    }
+}
